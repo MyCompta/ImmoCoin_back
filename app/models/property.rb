162cfa5a484d9223050fe56915b4ec566595e524
totycos <dv94@hotmail.fr>
2024-03-12 00:40:31 +0100

    test: Add Property tests model

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,6 +2,6 @@ class Property < ApplicationRecord
   belongs_to :user
 
   validates :title, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { only_integer: true }
   validates :description, presence: true
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,4 +1,5 @@
-# spec/models/user_spec.rb
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
